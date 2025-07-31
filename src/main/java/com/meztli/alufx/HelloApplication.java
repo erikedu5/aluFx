9b1aba64cfb418c76c9a769dbc7137af55949f5f
Erik Jimenez <erikedu5@gmail.com>
2025-07-30 21:26:28 -0600

    fix githubaction

--- src/main/java/com/meztli/alufx/HelloApplication.java
+++ src/main/java/com/meztli/alufx/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.meztli.alufx;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        URL fxml = HelloApplication.class.getResource("main-view.fxml");
+        if (fxml == null) {
+            System.err.println("❌ FXML no encontrado");
+            throw new RuntimeException("FXML no encontrado");
+        } else {
+            System.out.println("✅ FXML encontrado en: " + fxml);
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(fxml);
         Scene scene = new Scene(fxmlLoader.load(), 500, 600);
         stage.setTitle("Alu-Helper");
         stage.setScene(scene);
