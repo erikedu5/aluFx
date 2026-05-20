8e6f6158ccf8c946cfa049a3a4d5a8b1d8018591
Erik Jimenez <erikedu5@gmail.com>
2026-05-19 18:14:58 -0600

    chore: migrate MariaDB data directory to AppData and ensure user password reset in init script

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS aluhelper CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'alufx_user'@'localhost' IDENTIFIED BY 'root';
+ALTER USER 'alufx_user'@'localhost' IDENTIFIED BY 'root';
 
 GRANT ALL PRIVILEGES ON aluhelper.* TO 'alufx_user'@'localhost';
 
--- scripts/start-mariadb.bat
+++ scripts/start-mariadb.bat
@@ -1,9 +1,25 @@
 @echo off
-REM Launch bundled MariaDB using the provided configuration
-cd /d "%~dp0..\mariadb"
-if not exist "data\mysql" (
-    echo Initializing MariaDB database...
-    bin\mysqld --defaults-file=my.ini --initialize-insecure
+REM Launch bundled MariaDB using a user-writable local AppData directory to avoid Program Files permissions issues
+set "DB_DATA_DIR=%LOCALAPPDATA%\aluFx\data"
+set "DB_DATA_DIR=%DB_DATA_DIR:\=/%"
+
+set "INIT_SQL=%~dp0init.sql"
+set "INIT_SQL=%INIT_SQL:\=/%"
+
+REM Asegurar que el directorio de datos existe
+set "DB_DATA_DIR_WIN=%LOCALAPPDATA%\aluFx\data"
+if not exist "%DB_DATA_DIR_WIN%" (
+    echo Creating database data directory at "%DB_DATA_DIR_WIN%"...
+    mkdir "%DB_DATA_DIR_WIN%"
 )
+
+cd /d "%~dp0..\mariadb"
+
+REM Si la carpeta mysql no existe dentro del directorio de datos, inicializarla
+if not exist "%DB_DATA_DIR_WIN%\mysql" (
+    echo Initializing MariaDB database in "%DB_DATA_DIR%"...
+    bin\mysqld --defaults-file=my.ini --datadir="%DB_DATA_DIR%" --log-error="%DB_DATA_DIR%/mysqld.err" --pid-file="%DB_DATA_DIR%/mysqld.pid" --initialize-insecure
+)
+
 echo Starting MariaDB server...
-bin\mysqld --defaults-file=my.ini --init-file="%~dp0init.sql" --console
+bin\mysqld --defaults-file=my.ini --datadir="%DB_DATA_DIR%" --log-error="%DB_DATA_DIR%/mysqld.err" --pid-file="%DB_DATA_DIR%/mysqld.pid" --init-file="%INIT_SQL%" --console
