11bc7c13446a2a5d5b158e0b1df6009030ee7ac2
Erik Jimenez <erikedu5@gmail.com>
2026-05-19 20:10:36 -0600

    feat: add database user privileges for 127.0.0.1 host in init script

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,7 +2,10 @@ CREATE DATABASE IF NOT EXISTS aluhelper CHARACTER SET utf8mb4 COLLATE utf8mb4_un
 
 CREATE USER IF NOT EXISTS 'alufx_user'@'localhost' IDENTIFIED BY 'root';
 ALTER USER 'alufx_user'@'localhost' IDENTIFIED BY 'root';
-
 GRANT ALL PRIVILEGES ON aluhelper.* TO 'alufx_user'@'localhost';
+
+CREATE USER IF NOT EXISTS 'alufx_user'@'127.0.0.1' IDENTIFIED BY 'root';
+ALTER USER 'alufx_user'@'127.0.0.1' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON aluhelper.* TO 'alufx_user'@'127.0.0.1';
 
 FLUSH PRIVILEGES;
